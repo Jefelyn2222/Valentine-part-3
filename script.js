18557cfcdc98b66aac69c1709053295c557d83e1
Jefelyn2222 <hskshejejeje@gmail.com>
2025-02-13 21:39:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function openEnvelope() {
-    document.getElementById("letter").style.display = "block";
+    document.querySelector(".envelope-img").style.transform = "rotateX(180deg)";
+    document.querySelector(".letter").style.transform = "translateY(-10px)";
+    document.querySelector(".letter").style.opacity = "1";
+    document.querySelector(".flower").style.opacity = "1";
+    document.querySelector(".flower").style.transform = "scale(1)";
 }
